0b9b5ba8808674e716315c28c167dab9c2ec492c
akshay bhat <akshayubhat@gmail.com>
2018-02-18 21:38:03 -0800

    Add validation on pull request to VDN.
    Eventually:
    
    - Export models as a zip file.
    
    - Implement training detection model using YOLO
    
    - Implement location net and clustering using PQKMeans
    
    - Implement fine-tuning / training face recognition models
    
    - Pre-condition checks, and failed to launch error messages for processes.

--- validate.py
+++ validate.py
@@ -2,7 +2,9 @@ import os, json
 from dvaclient import schema
 
 if __name__ == '__main__':
+    print "Starting validation"
     for root, directories, filenames in os.walk(os.path.join(os.path.dirname(__file__),'.')):
+        print root, directories, filenames
         for filename in filenames:
             if filename.endswith('.json'):
                 print "Validating {}".format(filename)
